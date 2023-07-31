e3248d58bb3588ece4c0ad36b26765f05f50475f
Gaspar Sio <sio.gaspar@gmail.com>
2023-07-31 20:48:07 +0200

    3 new sliders, 1 video section

--- home.js
+++ home.js
@@ -29,7 +29,7 @@ const clickHeaderSecIcon = headerSecIcon.addEventListener('click', () => {
 })
 
 
-//SLIDER
+//SLIDER ROOM
 const slider = document.querySelector(".slider-images");
 const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
@@ -51,3 +51,50 @@ function updateSliderPosition() {
   const translateXValue = -currentIndex * slideWidth;
   slider.style.transform = `translateX(${translateXValue}px)`;
 }
+
+
+//SLIDER FEATURES CARDS
+const cardsContainer = document.querySelector(".cards-container");
+const prevBtn1 = document.querySelector(".prev-btn1");
+const nextBtn1 = document.querySelector(".next-btn1");
+
+let cardIndex = 0;
+const numCards = document.querySelectorAll(".card").length;
+
+function showCard(index) {
+    const cards = document.querySelectorAll(".card");
+    cardsContainer.style.transform = `translateX(-${index * 320}px)`;
+}
+
+prevBtn1.addEventListener("click", () => {
+  cardIndex = (cardIndex - 1 + numCards) % numCards;
+  showCard(cardIndex);
+});
+
+nextBtn1.addEventListener("click", () => {
+    cardIndex = (cardIndex + 1) % numCards;
+    showCard(cardIndex);
+});
+
+//SLIDER FOOD
+const cardsContainer2 = document.querySelector(".food__cards-container");
+const prevBtn2 = document.querySelector(".prev-btn2");
+const nextBtn2 = document.querySelector(".next-btn2");
+
+let cardIndex2 = 0;
+const numCards2 = document.querySelectorAll(".card-food").length;
+
+function showCardFood(index) {
+    const cards = document.querySelectorAll(".card-food");
+    cardsContainer2.style.transform = `translateX(-${index * 320}px)`;
+}
+
+prevBtn2.addEventListener("click", () => {
+  cardIndex2 = (cardIndex2 - 1 + numCards2) % numCards2;
+  showCardFood(cardIndex2);
+});
+
+nextBtn2.addEventListener("click", () => {
+    cardIndex2 = (cardIndex2 + 1) % numCards2;
+    showCardFood(cardIndex2);
+});
