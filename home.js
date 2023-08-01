a7e8f4abaf0bfcb31b8da573783a4788a9aefa30
Gaspar Sio <sio.gaspar@gmail.com>
2023-08-01 20:07:50 +0200

    last sections on home.html, footer, new sections on about.html

--- home.js
+++ home.js
@@ -1,34 +1,3 @@
-//CLICK HAMBURG MENU
-const headerSecIcon = document.querySelector('.header__section-ham-menu')
-const mobImg = document.querySelector('.icon-menu--big');
-const crossIcon = document.querySelector('.icon-menu--cross');
-const header = document.querySelector('.header__section');
-const crossImg = document.querySelector('.crossIcon');
-const menuMovil = document.querySelector('.header__mob-menu');
-
-function expandirHeader() {
-    header.classList.add('header-expand');
-    menuMovil.classList.remove('inactive')
-}
-function cerrarHeader() {
-    header.classList.remove('header-expand');
-    menuMovil.classList.add('inactive')
-}
-const clickHeaderSecIcon = headerSecIcon.addEventListener('click', () => {
-    mobImg.classList.toggle('inactive');
-    crossIcon.classList.toggle('inactive');
-    
-    const hambInact = mobImg.classList.contains('inactive');
-    const crossInact = crossIcon.classList.contains('inactive');
-
-    if(hambInact){
-        expandirHeader()
-    }else{
-        cerrarHeader()
-    }
-})
-
-
 //SLIDER ROOM
 const slider = document.querySelector(".slider-images");
 const prevBtn = document.querySelector(".prev-btn");
